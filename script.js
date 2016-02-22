e763b429f902cf4530940503c5ceb4a187d3df5f
Mahima <mahima@gmail.com>
2016-02-22 15:09:31 +0530

    changed

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 $(document).ready(function() {
 $('.down-arrow').click(function() {
 $('html, body').animate({
-scrollTop: $('.c1').height()-$('.p1').height()
+scrollTop: $('.c1').height() - $('.p1').height()
 }, 2000);
 });
 });
 
-
-/*$(document).ready(function() {
-   
-    $('#someID').click(function(){
-        $('html, body').animate({scrollTop:$(document).height()}, 'slow');
-        return false;
-    });
-
-});*/
