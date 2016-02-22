fddea3df2cec80ad03648f747449d03d362afd5d
Mahima <mahima@gmail.com>
2016-02-22 17:58:59 +0530

    scrollBy

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
 $('.down-arrow').click(function() {
 $('html, body').animate({
 scrollTop: $('.header1').height() 
 }, 2000);
 });
-});
+});*/
 
+
+function scrollWin() {
+    window.scrollBy(0, 500);
+}
