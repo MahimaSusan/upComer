dd2f5b36b1f7a778b90fcdcbe03cec13c37d0de3
Mahima <mahima@gmail.com>
2016-02-22 16:20:01 +0530

    sroll

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 $('.down-arrow').click(function() {
 $('html, body').animate({
-scrollTop: $('.c1').height() - $('.p1').height()
-}, 1000);
+scrollTop: $('.header1').height() 
+}, 2000);
 });
 });
 
